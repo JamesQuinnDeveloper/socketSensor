4b05ffab2b3cb4b67acef0abeefddf904246304e
james <jamesquinndeveloper@gmail.com>
2019-10-11 20:03:26 -0400

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var server = app.listen(8989,function(){
+    console.log('listening on 8989!')
+}); 
+
+// Static files
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection',function(socket){
+    console.log('made socket connection', socket.id)
+});

6d83ed240e4fff01c2180cf45bbd7b726fba426f
james <jamesquinndeveloper@gmail.com>
2019-10-11 22:24:21 -0400

    update

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ app.use(express.static('public'));
 
 var io = socket(server);
 
+
 io.on('connection',function(socket){
     console.log('made socket connection', socket.id)
 });
+
+
+const readSensor = () => {
+	// read sensor data here ***
+	const temp = readTemp();
+​
+	io.emit('new-data', {
+		temp
+	})
+}
+
+setTimeout(readSensor, 2000);
--- public/client.js
+++ public/client.js
@@ -1,3 +1,15 @@
 //Make Connection
 
 var socket = io.connect('http://localhost:8989');
+
+const cb = payload => {
+	const { temp, humidity } = payload;
+	document.findElementById('temp').value = temp;
+	document.findElementById('humid').value = humidity;
+}
+​
+socket.on('new-data', cb);
+​
+​
+
+
